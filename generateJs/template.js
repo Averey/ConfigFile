ef1b4eb53be247ca505b9fdd6e778a6b89afb5c1
linclon <284946608@qq.com>
2017-04-06 18:35:47 +0800

    jj

---
+++ generateJs/template.js
@@ -0,0 +1,15 @@
+(function(){
+    'use strict';
+    angular
+        .module('$$$Module')
+        .component('$$$', {
+            templateUrl: '',
+            controller: $$$Controller,
+            controllerAs: 'vm'
+        });
+
+    /**@ngInject*/
+    function $$$Controller($scope){
+
+    }
+})();
